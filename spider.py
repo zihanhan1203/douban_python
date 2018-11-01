480e704aa1b97a19488397e10ddc2c34fc4124a7
JinhaoHu <hujinhao0401@gmail.com>
2018-11-01 00:01:55 -0400

    也是把拿页面源码的代码拿出来

--- spider.py
+++ spider.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
-import urllib2
+import get_data_from_url
 from lxml import etree
 import comment_spider
-import time
 
 
 class PageSpider(object):
@@ -21,63 +19,10 @@ class PageSpider(object):
         return self.get_into_topics()
 
     def get_into_topics(self):
-        # driver = webdriver.Chrome()
-        # driver.get(self.topic_url)
-        # proxies = {"https": "118.178.227.171:80", "http": "115.218.210.189:9000"}
-        # s = requests.session()
-        # s.keep_alive = False
-        # response = requests.get(self.topic_url, proxies=proxies)
-        # html = self.encode_html(response.text)
-        # driver.close()
-        # print html
-        # httpproxy_handler = urllib2.ProxyHandler({"https": "118.178.227.171:80"})
         set_of_post = set()  # ATTENTION the way to create empty set
-        proxies = {}
-        file_path = './ip.txt'
-        file_ob = open(file_path, 'r')
-        res_ip = file_ob.readlines()  # get proxy ips
-        file_ob.close()
-        file_write_ob = open(file_path, 'w')
-        set_of_failed_ip = []
-        the_index_of_useful_ip = 0
-        httpproxy_handler = urllib2.ProxyHandler({})
-        headers = {'User-agent': 'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:22.0) Gecko/20100101 Firefox/22.0'}
-        request = urllib2.Request(self.topic_url, None, headers=headers)
-        opener = urllib2.build_opener(httpproxy_handler)
-        for i in range(51):
-            try:
-                print "Try to get the urls of the post in page %s" % self.topic_url
-                response = opener.open(request, timeout=5)
-                content = response.read()
-                response.close()
-            except Exception, e:
-                if i >= 50:
-                    print str(e)
-                    for ip in res_ip:
-                        file_write_ob.write(ip)
-                    file_write_ob.close()
-                    return set_of_post
-                else:
-                    if i > 0:
-                        set_of_failed_ip.append(res_ip[i-1])
-                    proxies['https'] = res_ip[i].strip('\n')  # change ip
-                    opener = urllib2.build_opener(urllib2.ProxyHandler(proxies))
-                    time.sleep(0.5)
-            else:
-                if i > 0:
-                    the_index_of_useful_ip = i-1
-                print "Success!!!"
-                opener.close()
-                time.sleep(0.1)
-                break
-
-        for i in range(the_index_of_useful_ip, 50):
-            file_write_ob.write(res_ip[i])
-        for i in set_of_failed_ip:
-            file_write_ob.write(i)
-        file_write_ob.close()
-
-        html = self.encode_html(content)
+        get_data = get_data_from_url.GetDataFromUrl(self.topic_url)
+        content = get_data.get_data()
+        html = content.decode('utf8')
         tree = etree.HTML(html)
         urls = tree.xpath("//*[@id='content']//tr/td[@class='title']/a[@href]/@href")
         fetch_each_comment = comment_spider.CommentSpider()
@@ -100,6 +45,3 @@ class PageSpider(object):
                     file_object.write(each_url + '\n')
                     file_object.close()
         return set_of_post
-
-    def encode_html(self, html):
-        return html.decode('utf8')
